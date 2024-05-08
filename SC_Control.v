450ba86270d9ac3a75d9d5bdc204dedfe7cd7aa3
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-05-08 23:16:28 +0300

    Delete SC_Control.v

--- SC_Control.v
+++
@@ -1,8 +0,0 @@
-module SC_Control ( 
-   T, D, I,CLR 
-); 
-input  I; 
-input [7:0] T, D; 
-output  CLR; 
-assign CLR = ((D[7] & (~I) & T[3]) | ((D[0] | D[1] | D[2] | (D[5])) & T[5]) | (D[3] & T[4]) | (D[4] & T[4]) | (D[6] & T[6]));
-endmodule
