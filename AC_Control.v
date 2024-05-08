f874ea249dcd8834d9071f0439b4daba9a116e23
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-05-08 23:15:52 +0300

    Delete AC_Control.v , AR_Control.v , DR_Control.v , IR_Control.v , PC_Control.v , SC_Control.v

--- AC_Control.v
+++
@@ -1,9 +0,0 @@
-module AC_Control ( 
-    input [7:0] T, input [7:0] D ,input I, input [7:0] B, 
-    output  LD,CLR,INR 
-); 
-assign LD = (T[5] & (D[0] | D[1] | D[2])) | ((D[7] & !I & T[3]) & B[2]) | (D[5] & T[5]); //B[9]
-assign CLR = (D[7] & !I & T[3]) & B[3]; //B[11]
-assign INR = (D[7] & !I & T[3]) & B[0]; //B[5]
-
-endmodule
