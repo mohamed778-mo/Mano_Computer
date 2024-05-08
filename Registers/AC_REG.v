60a00130a6ad56eda0c580f3caadfbc8da856463
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-05-09 00:58:49 +0300

    Update AC_REG.v

--- Registers/AC_REG.v
+++ Registers/AC_REG.v
@@ -1,19 +1,21 @@
 module AC_Reg( 
-    input INR, clk, LD, CLR,
-    input [7:0] in,
-    output reg [7:0] out_ac
-); 
-initial begin 
-    out_ac = 8'hac; 
-end 
+    input INR,clk ,LD,CLR
+    
+    ,input[7:0] in 
+    ,output reg [7:0] out_ac 
+    
+) ; 
 
-always @(posedge clk) begin 
-    if (CLR) begin 
-        out_ac <= 8'b0; 
-    end else if (LD) begin 
-        out_ac <= in; 
-    end else if (INR) begin 
-        out_ac <= in + 1; 
+initial begin 
+     out_ac=8'hac; 
+ 
+ end 
+ 
+
+    always @(posedge clk ) begin 
+         if(CLR) begin out_ac<=8'b0; end
+         else if (LD) begin out_ac <=in; end
+         else if (INR) begin  out_ac<=out_ac+1; end
+ 
     end
-end
 endmodule
